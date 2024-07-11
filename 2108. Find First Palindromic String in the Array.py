356d0abc5d4b2494ca710a838ebb0721ed8d29be
harshr2135 <harshithm2135@outlook.com>
2024-07-11 19:36:15 +0530

    Create 2108. Find First Palindromic String in the Array.py

---
+++ 2108. Find First Palindromic String in the Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack = []
+        ans = []
+
+        for c in s:
+            if c == '(':
+                stack.append(len(ans))
+            elif c == ')':
+                j = stack.pop()
+                ans[j:] = ans[j:][::-1]
+            else:
+                ans.append(c)
+
+        return ''.join(ans)
