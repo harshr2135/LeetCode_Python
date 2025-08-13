9c93d7baddc0513715bad32bf0e4cc95f23a7298
harshr2135 <harshithm2135@outlook.com>
2025-08-13 19:59:25 +0530

    Update 0141. Linked List Cycle.py

--- 0141. Linked List Cycle.py
+++ 0141. Linked List Cycle.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
@@ -13,4 +13,5 @@ class Solution:
             fast = fast.next.next
             if slow == fast:
                 return True
+
         return False
