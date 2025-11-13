a13aabdad307d11c0dfbbb7c20a270ac00a32d71
harshr2135 <harshithm2135@outlook.com>
2025-11-13 19:16:50 +0530

    Update 0287. Find the Duplicate Number.py

--- 0287. Find the Duplicate Number.py
+++ 0287. Find the Duplicate Number.py
@@ -1,9 +1,9 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        freq = [0] * (len(nums) + 1)
-        for i in range(len(nums)):
-            if freq[nums[i]] == 0:
-                freq[nums[i]] += 1
-            else:
+        nums.sort()
+
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
                 return nums[i]
-        return 0
+
+        return -1
