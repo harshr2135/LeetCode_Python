0dd1b084db7485ffe8c71b8060a4f63acae69f5e
harshr2135 <harshithm2135@outlook.com>
2026-01-13 19:50:01 +0530

    Update 1346. Check if N and Its Double Exist.py

--- 1346. Check if N and Its Double Exist.py
+++ 1346. Check if N and Its Double Exist.py
@@ -1,12 +1,10 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        for i in range(len(arr)-1):
-            for j in range(i+1, len(arr)):
-                if arr[j] == 0 and arr[i] == 0:
-                    return True
-                elif arr[j] == 0 or arr[i] == 0:
-                    continue
-                elif arr[i]/arr[j]==2 or arr[j]/arr[i]==2:
-                    return True
+        seen = set()
+
+        for num in arr:
+            if num * 2 in seen or (num % 2 == 0 and num // 2 in seen):
+                return True
+            seen.add(num)
 
         return False
