a0b1b6c1963cf0bbc0ca8cc8879aed5a235a82de
harshr2135 <harshithm2135@outlook.com>
2026-01-13 22:11:02 +0530

    Update 0069. Sqrt(x).py

--- 0069. Sqrt(x).py
+++ 0069. Sqrt(x).py
@@ -1,3 +1,17 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(x**0.5)
+        left = 0
+        right = x
+
+        while left <= right:
+            mid = (left+right)//2
+            if mid*mid == x:
+                return mid
+
+            elif mid*mid > x:
+                right = mid - 1
+        
+            else:
+                left = mid + 1
+        
+        return right
