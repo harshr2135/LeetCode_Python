62d05e29e69b5b52fc1e2e5996519aa93ef2f03b
harshr2135 <harshithm2135@outlook.com>
2026-01-20 21:57:24 +0530

    Update 0049. Group Anagrams.py

--- 0049. Group Anagrams.py
+++ 0049. Group Anagrams.py
@@ -1,11 +1,9 @@
-from collections import defaultdict
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagram_map = defaultdict(list)
-    
-        for word in strs:
-            key = ''.join(sorted(word))  # sort characters in the word
-            anagram_map[key].append(word)
-    
-        return list(anagram_map.values())
+        map = defaultdict(list)
+
+        for s in strs:
+            key = "".join(sorted(s))
+            map[key].append(s)
+
+        return list(map.values())
