828b9e598181db2f7ac962058a7ef0f3288e6474
harshr2135 <harshithm2135@outlook.com>
2026-01-26 17:43:04 +0530

    Update 0078. Subsets.py

--- 0078. Subsets.py
+++ 0078. Subsets.py
@@ -1,16 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        subset = []
 
-        def dfs(i):
-            if i >= len(nums):
-                res.append(subset.copy())
+        def backtrack(idx, curr):
+            if idx == len(nums):
+                res.append(curr.copy())
                 return
-            subset.append(nums[i])
-            dfs(i + 1)
-            subset.pop()
-            dfs(i + 1)
 
-        dfs(0)
+            backtrack(idx+1, curr)
+
+            curr.append(nums[idx])
+            backtrack(idx+1, curr)
+
+            curr.pop()
+
+        backtrack(0, [])
         return res
