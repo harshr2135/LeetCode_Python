0b2f73fa0424681c62104aeb5d80d34e8593e6bf
harshr2135 <harshithm2135@outlook.com>
2026-01-27 20:07:06 +0530

    Update 0020. Valid Parentheses.py

--- 0020. Valid Parentheses.py
+++ 0020. Valid Parentheses.py
@@ -12,3 +12,26 @@ class Solution:
                 stack.pop()
 
         return not stack
+
+
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+
+#         for brac in s:
+#             if brac in ['(', '{', '[']:
+#                 stack.append(brac)
+
+#             else:
+#                 if not stack:
+#                     return False
+#                 if brac == ')' and stack[-1] == '(':
+#                     stack.pop()
+#                 elif brac == '}' and stack[-1] == '{':
+#                     stack.pop()
+#                 elif brac == ']' and stack[-1] == '[':
+#                     stack.pop()
+#                 else:
+#                     return False
+
+#         return not stack
