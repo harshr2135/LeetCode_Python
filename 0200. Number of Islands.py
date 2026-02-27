5199c657262bcbd717eab708c452d66b187bc5a9
harshr2135 <harshithm2135@outlook.com>
2026-02-27 09:56:40 +0530

    Create 0200. Number of Islands.py

---
+++ 0200. Number of Islands.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        row, col = len(grid), len(grid[0])
+
+        def dfs (grid, r, c):
+
+            if r<0 or c<0 or r >= row or c >= col or grid[r][c]=='0':
+                return 
+            
+            grid[r][c] = '0'
+
+            dfs(grid, r+1, c)
+            dfs(grid, r, c+1)
+            dfs(grid, r-1, c)
+            dfs(grid, r, c-1)
+
+        count = 0
+
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == '1':
+                    count += 1
+                    dfs(grid, r, c)
+
+        return count
