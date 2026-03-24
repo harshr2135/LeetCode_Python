b0b0ef8342a935ec4d295f82c0719b50ce3b1622
harshr2135 <harshithm2135@outlook.com>
2026-03-24 07:20:52 +0530

    Update 0098. Validate Binary Search Tree.py

--- 0098. Validate Binary Search Tree.py
+++ 0098. Validate Binary Search Tree.py
@@ -1,3 +1,4 @@
+'''STACK APPROACH'''
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -21,3 +22,24 @@ class Solution:
             root = root.right
 
         return True
+    
+
+'''RECURSIVE (DFS) APPROACH'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs (node, low, high):
+            if not node:
+                return True
+
+            if not (low < node.val < high):
+                return False
+
+            return dfs(node.left, low, node.val) and dfs(node.right, node.val, high)
+
+        return dfs(root, float(-inf), float(inf))
